3375255709900f37c8f0437dd09fc50393d6dcbf
karimorozova13 <k.morozova@tiomarkets.com>
2023-10-24 10:36:51 +0300

    finish home work with collections

--- collections/command_line.py
+++ collections/command_line.py
@@ -6,3 +6,9 @@ for arg in sys.argv:
 def main():
     print(sys.argv[0])
 main()
+def parse_args():
+    result = ""
+    result = ' '.join(sys.argv[1::])
+    
+    return result
+print(parse_args())
--- collections/practice.py
+++ collections/practice.py
@@ -1,4 +1,5 @@
 from random import randint
+import re
 
 def amount_payment(payment):
     total =0
@@ -121,5 +122,36 @@ def get_random_password(password = ''):
 
     
 print(get_random_password())
+
+def is_valid_password(password):
+    regex = ("^(?=.*[a-z])(?=." +
+             "*[A-Z])(?=.*\\d)" )
+     
+    p = re.compile(regex)
+    if len(password) <8 or not re.search(p, password):
+        return False
+    
+    return True
+
+    # BETTER SOLUTION
+    # if len(password) != 8:
+    #     return False
+
+    # has_upper = False
+    # has_lower = False
+    # has_num = False
+
+    # for ch in password:
+    #     if ch.isupper():
+    #         has_upper = True
+    #     elif ch.islower():
+    #         has_lower = True
+    #     elif ch.isdigit():
+    #         has_num = True
+
+    # return has_upper and has_lower and has_num
+
+print(is_valid_password('b8g^ro4^'))
+
 
 
