b938a867ff3460d878dd028ce0d1759a7e49dfc8
karimorozova13 <k.morozova@tiomarkets.com>
2023-11-08 17:53:15 +0200

    task4

--- module7/practice/task4.py
+++ module7/practice/task4.py
@@ -1,8 +1,7 @@
 def add_employee_to_file(record, path):
     fh= open(path, 'a')
-    fh.write('\n')
 
-    fh.write(record)
+    fh.write(f'{record}\n')
     fh.close()
     return True
-print(add_employee_to_file("nfdsfdsfew gfdgdfg,19",'new_list.txt'))
+print(add_employee_to_file("new line,19",'new_list.txt'))
